e24233f006479b002822968d5f580ed093604e50
aleks-rosca <56295581+aleks-rosca@users.noreply.github.com>
2021-05-30 11:33:47 +0200

    update

--- function_1/main.py
+++ function_1/main.py
@@ -25,7 +25,7 @@ def get_last20Years(request):
     pool_recycle=1800
   )
   result = []
-  years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+  years = range(2000, 2022)
   for year in years:       
       query = "SELECT COUNT(*) FROM movies WHERE year = " + f'{year}'
       with db.engine.begin() as conn:
--- function_2/main.py
+++ function_2/main.py
@@ -25,7 +25,7 @@ def get_rating(request):
     pool_recycle=1800
   )
   result = []
-  years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021]
+  years = range(2000, 2022)
   for year in years:       
       query = "SELECT AVG(rating) FROM ratings INNER JOIN movies ON (ratings.movie_id = movies.id) where movies.year = " + f'{year}'
       with db.engine.begin() as conn:
